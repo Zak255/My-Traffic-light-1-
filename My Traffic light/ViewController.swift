cdeedd48347ad6fae0c7109b585a8692fd102f80
Zakir.Kazimov <ramzan666666@gmail.com>
2021-07-08 19:28:09 +0300

    Initial Commit

---
+++ My Traffic light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Traffic light
+//
+//  Created by Zakir on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var redButton: UIView!
+    @IBOutlet weak var yellowButton: UIView!
+    @IBOutlet weak var greenButton: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func buttonStart(_ sender: Any) {
+       
+    }
+    
+}
+
