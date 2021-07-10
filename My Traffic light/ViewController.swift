04c4be4f9da2cb5106c3ebc4c81599447fe0db0f
Zakir.Kazimov <ramzan666666@gmail.com>
2021-07-10 19:57:10 +0300

    2

--- My Traffic light/ViewController.swift
+++ My Traffic light/ViewController.swift
@@ -1,25 +1,63 @@
-//
-//  ViewController.swift
-//  My Traffic light
-//
-//  Created by Zakir on 08.07.2021.
-//
-
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
+
 class ViewController: UIViewController {
-    @IBOutlet weak var redButton: UIView!
-    @IBOutlet weak var yellowButton: UIView!
-    @IBOutlet weak var greenButton: UIView!
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        print ("Размер стороны, доступный в методе vievDidLoad: \(redLight.frame.height)")
     }
 
-    @IBAction func buttonStart(_ sender: Any) {
-       
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        
+        print("Размер стороны, доступный в методе viewWillLayoutSubviews: \(redLight.frame.height)")
+    }
+    
+    @IBAction func startButtonPressed () {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+            }
+    
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
     }
     
 }
-
+ 
